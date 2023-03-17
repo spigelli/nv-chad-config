657aaf3ec8218da70fccb476be945fe6d1565d8d
Isaac Spiegel <isaacspiegel@gmail.com>
2023-03-17 13:44:09 -0500

    Add some mappings

--- mappings.lua
+++ mappings.lua
@@ -14,10 +14,72 @@ M.general = {
 M.nvimtree = {
   plugin = true,
   n = {
+    -- Pressing Cmd-E toggles the file picker
     ["<D-e>"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
   },
   i = {
     ["<D-e>"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+  },
+}
+
+M.tabufline = {
+  n = {
+    ["<D-S-]>"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflineNext()
+      end,
+      "goto next buffer",
+    },
+    ["<D-S-[>"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflinePrev()
+      end,
+      "goto prev buffer",
+    },
+    ["<D-w>"] = {
+      function()
+        require("nvchad_ui.tabufline").close_buffer()
+      end,
+      "close buffer",
+    },
+  },
+  i = {
+    ["<D-S-]>"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflineNext()
+      end,
+      "goto next buffer",
+    },
+    ["<D-S-[>"] = {
+      function()
+        require("nvchad_ui.tabufline").tabuflinePrev()
+      end,
+      "goto prev buffer",
+    },
+    -- close buffer + hide terminal buffer
+    ["<D-w>"] = {
+      function()
+        require("nvchad_ui.tabufline").close_buffer()
+      end,
+      "close buffer",
+    },
+  },
+}
+
+M.comment = {
+  n = {
+    ["<D-/>"] = {     
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "toggle comment",
+    },
+  },
+  v = {
+    ["<D-/>"] = {
+      "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+      "toggle comment",
+    },
   },
 }
 
